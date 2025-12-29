818ad9e9f5b7b27c7cb5c6cdea1eefce9246e4d2
Conner Moulaison <csmoulaison@gmail.com>
2025-12-28 22:17:10 -0800

    Rename to submarine in build.sh

--- build/build.sh
+++ build/build.sh
@@ -5,7 +5,7 @@ cp -r ../src/shaders ../bin/
 ./fonts/atlas ./fonts/Iceland-Regular.ttf ../bin/fonts/font_small.cmfont 64 > /dev/null
 ./fonts/atlas ./fonts/Iceland-Regular.ttf ../bin/fonts/font_large.cmfont 108 > /dev/null
 
-g++ -g -o ../bin/battleship_4d \
+g++ -g -o ../bin/submarine \
 	../src/game/main.cpp ../src/window/xlib/xlib_window.cpp ../src/time/unix/unix_time.cpp ../src/renderer/opengl/opengl.cpp \
 	../src/renderer/opengl/GL/gl3w.c \
 	-I ../src/ \
